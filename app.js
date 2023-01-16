7ac8b4efdc38ef4e18a44ef3bfe33f15d6f131e4
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2023-01-16 15:22:39 -0500

    changed a thing

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get('/', (req, res) => res.type('html').send(html))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 const html = `
 <!DOCTYPE html>
@@ -52,7 +51,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Eat shit MOTHERFUCKERS!
     </section>
   </body>
 </html>
